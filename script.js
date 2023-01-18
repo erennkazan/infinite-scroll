f6eb6bf41293762aff99d6e10a69fafaab22a4da
erennkazan <erennkazan@gmail.com>
2023-01-19 00:12:41 +0300

    code edit

--- script.js
+++ script.js
@@ -9,16 +9,27 @@ let photosArray = [];
     const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 
+    function setAttributes(element, attributes){
+        for(const key in attributes){
+            element.setAttribute(key, attributes[key]);
+        }
+    }
+
+
     //create elements for links & photos add to dom
     function displayPhotos(){
         photosArray.forEach((photo) => {
             const item = document.createElement('a');
-            item.setAttribute('href', photo.links.html);
-            item.setAttribute('target', '_blank');
+            setAttributes(item, {
+                href: photo.links.html,
+                target: '_blank',
+            });
             const img = document.createElement('img');
-            img.setAttribute('src', photo.urls.regular);
-            img.setAttribute('alt', photo.alt_description);
-            img.setAttribute('title', photo.alt_description);
+            setAttributes(img,  {
+                src: photo.urls.regular,
+                alt: photo.alt_description,
+                title: photo.alt_description,
+            });
             item.appendChild(img);
             imageContainer.appendChild(item);
       }); 

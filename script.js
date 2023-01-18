6eb60bc5bd9d5876688208428fc4d7efc2960b0e
erennkazan <erennkazan@gmail.com>
2023-01-19 00:26:25 +0300

    infinite photo added

--- script.js
+++ script.js
@@ -47,5 +47,11 @@ let photosArray = [];
 
         }
     }
+    window.addEventListener('scroll', () =>{
+        if(window.innerHeight + window.scrollY >= document.body.offsetHeight -1000){
+            getPhotos();
+            console.log('load more');
 
+        }
+    });
     getPhotos();

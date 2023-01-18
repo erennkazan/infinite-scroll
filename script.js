da0657ec444a01d5e0501b46d72b2b72dd98e37c
erennkazan <erennkazan@gmail.com>
2023-01-19 00:05:08 +0300

    where the photos are displayed

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
-    // unsplash api
+let photosArray = [];
+
+// unsplash api
     const count = 10;
     const apiKey = 't0TM3SY7BL_R1dxVJMBPOfwBGwmig_OCgyELh73oGVg';
     const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+    //create elements for links & photos add to dom
+    function displayPhotos(){
+        photosArray.forEach((photo) => {
+            const item = document.createElement('a');
+            item.setAttribute('href', photo.links.html);
+            item.setAttribute('target', '_blank');
+            const img = document.createElement('img');
+            img.setAttribute('src', photo.urls.regular);
+            img.setAttribute('alt', photo.alt_description);
+            img.setAttribute('title', photo.alt_description);
+            item.appendChild(img);
+            imageContainer.appendChild(item);
+      }); 
+    }
 
 
     //get photos from unsplash api
@@ -10,8 +30,8 @@
     async function getPhotos(){
         try{
             const response = await fetch(apiUrl);
-            const data = await response.json();
-            console.log(data);
+            photosArray = await response.json();
+            displayPhotos();
         }catch(error){
 
         }
